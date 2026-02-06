8c57f4c117d969fc54b7e0cca5189d174f691f7d
QueenCorina101 <clarkcorina1@gmail.com>
2026-02-05 23:51:02 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-# Mini Project — Decision Quiz
-#Student Name: 
+# Mini Project – Decision Quiz
+# Student Name: Corina Clark
 # Topic: If / Elif / Else
 # Goal: Create a quiz that gives different feedback based on the user's answer
 
 print("Welcome to the Decision Quiz!")
 print("Welcome to the Georgia Quiz!")
 
-
-print("What's the capitial of Georgia?")
+print("What's the capital of Georgia?")
 answer = input("Enter your answer: ")
+
 if answer == "Atlanta":
-print("Correct! Atlanta is the capitial of Georgia.")
-elif answer ==
-print("You're close, remember to capitalize the first letter.")
-else: print("You are incorrect, try again!")
+    print("Correct! Atlanta is the capital of Georgia.")
+elif answer == "atlanta":
+    print("You're close! Remember to capitalize the first letter.")
+else:
+    print("You are incorrect. Try again!")
